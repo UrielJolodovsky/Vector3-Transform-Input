8fa3ad5380af4f8bab61218dbf02322d7a0dba9c
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-05-20 08:38:03 -0300

    Transform, Scale y Rotation

---
+++ Assets/ObjectTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTransform : MonoBehaviour {
+
+    public GameObject myCube;
+    public Vector3 newScale;
+    // Use this for initialization
+	void Start ()
+    {
+        myCube.transform.localScale = newScale;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
