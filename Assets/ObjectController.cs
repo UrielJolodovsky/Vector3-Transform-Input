4c1600b23700376b92ff1180f44c0c0e8e678581
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-05-20 09:04:32 -0300

    Transform y Vector3

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+
+    public float xSpeed;
+
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.position.x <= 9)
+        {
+            transform.position += new Vector3(xSpeed,0,0);
+        }
+	}
+}
---
+++ Assets/ObjectMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+
+    public GameObject myCube;
+    public Vector3 rotationVector;
+    // Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myCube.transform.eulerAngles += rotationVector;
+    }
+}
